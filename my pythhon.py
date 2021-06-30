d83c680b6e90065c86615a735d3d281fda5f1ba8
Vivek Pandey <49920208+pandey269@users.noreply.github.com>
2021-06-30 17:54:27 +0530

    Add files via upload

---
+++ my pythhon.py
@@ -0,0 +1,31 @@
+def main(lst):
+    sub_lst=[[]]
+    count=0
+    for i in range(len(lst)+1):
+        for j in range(i+1,len(lst)+1):
+            sub=lst[i:j]
+            count=count+i
+            sub_lst.append(sub)
+    print(count)
+    return sub_lst    
+lst=[1,2,3,4,5]
+print(main(lst))
+
+
+#palindrome of string
+num=int(input())
+for i in range(0,num):
+    a=list(map(int,input("Enter a num").strip().split(" ")))[:num]
+print(a)
+    
+
+    
+
+
+
+
+
+
+
+
+
