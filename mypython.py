b6d42cb3d86ffba535984c1fcbbd6c513c7b2074
Vivek Pandey <49920208+pandey269@users.noreply.github.com>
2021-07-06 17:54:58 +0530

    Update mypython.py

--- mypython.py
+++ mypython.py
@@ -9,14 +9,12 @@ def main(lst):
     print(count)
     return sub_lst    
 lst=[1,2,3,4,5]
+print(lst)
+print("\n")
+print("It's subet...")
+print("\n")
 print(main(lst))
 
-
-#palindrome of string
-num=4
-for i in range(0,num):
-    a=list(map(int,input("Enter a num").strip().split(" ")))[:num]
-print(a)
     
 
     
